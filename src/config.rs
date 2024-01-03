87828a51f5bbc85f375f6a0fa76c93bc1c0ccd1f
alemi <me@alemi.dev>
2024-01-03 19:54:42 +0100

    fix: use serde_inline_default and serde_default
    now config behaves as expected, with fallback fields merged with the
    main struct, also less impl Default and values right next to the field

--- src/config.rs
+++ src/config.rs
@@ -1,85 +1,83 @@
+use serde_inline_default::serde_inline_default;
+// note that serde would want us to create a function for each immediate value we would want to set
+// as default. that gets VERY annoying VERY fast, and we have a lot of fields here.
+// i really dislike what serde-inline-default does, just adding more complexity making all these
+// immediate functions for me, but the code would be EXTREMELY bad to maintain otherwise
+//
+// track progress for official #[serde(default_value = ...)] here : https://github.com/serde-rs/serde/issues/368
 
 
-#[derive(Debug, Clone, Default, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, serde_default::DefaultFromSerde)]
 pub struct Config {
+	#[serde(default)]
 	pub overrides: ConfigOverrides,
 
+	#[serde(default)]
 	pub notifiers: ConfigNotifiers,
 
+	#[serde(default)]
 	pub template: ConfigTemplate,
 }
 
-#[derive(Debug, Clone, Default, serde::Serialize, serde::Deserialize)]
+#[serde_inline_default]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, serde_default::DefaultFromSerde)]
 pub struct ConfigOverrides {
+	#[serde(default)]
 	pub author: Option<String>,
 
+	#[serde(default)]
 	pub public: Option<bool>,
 
-	#[serde(default = "_true")]
+	#[serde_inline_default(true)]
 	pub date: bool,
 }
 
-
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[serde_inline_default]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, serde_default::DefaultFromSerde)]
 pub struct ConfigTemplate {
+	#[serde_inline_default("guestbook".into())]
 	pub title: String,
+
+	#[serde_inline_default("https://cdn.alemi.dev/social/someriver.jpg".into())]
 	pub logo: String,
+
+	#[serde_inline_default("you found my guestbook! please take a moment to sign it (:".into())]
 	pub description: String,
+
+	#[serde_inline_default("Kilroy was here  Ω".into())]
 	pub placeholder_body: String,
+
+	#[serde(default)]
 	pub canonical: Option<String>,
+
+	#[serde(default)]
 	pub palette: ConfigPalette,
 }
 
-impl Default for ConfigTemplate {
-	fn default() -> Self {
-		ConfigTemplate {
-			title: "guestbook.rs".into(),
-			logo: "https://cdn.alemi.dev/social/someriver.jpg".into(),
-			description: "you found my guestbook! please take a moment to sign it (:".into(),
-			placeholder_body: "Kilroy was here  Ω".into(),
-			canonical: None,
-			palette: ConfigPalette::default(),
-		}
-	}
-}
-
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[serde_inline_default]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, serde_default::DefaultFromSerde)]
 pub struct ConfigPalette {
+	#[serde_inline_default("#201F29".into())]
 	pub bg_main: String,
+	#[serde_inline_default("#292835".into())]
 	pub bg_off: String,
+	#[serde_inline_default("#E8E1D3".into())]
 	pub fg_main: String,
+	#[serde_inline_default("#ADA9A1".into())]
 	pub fg_off: String,
+	#[serde_inline_default("#BF616A".into())]
 	pub accent: String,
+	#[serde_inline_default("#824E53".into())]
 	pub accent_dark: String,
+	#[serde_inline_default("#D1888E".into())]
 	pub accent_light: String,
 }
 
-impl Default for ConfigPalette {
-	fn default() -> Self {
-		ConfigPalette {
-			bg_main: "#201F29".into(),
-			bg_off: "#292835".into(),
-			fg_main: "#E8E1D3".into(),
-			fg_off: "#ADA9A1".into(),
-			accent: "#BF616A".into(),
-			accent_dark: "#824E53".into(),
-			accent_light: "#D1888E".into(),
-		}
-	}
-}
-
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[serde_inline_default]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, serde_default::DefaultFromSerde)]
 pub struct ConfigNotifiers {
+	#[serde_inline_default(vec![NotifierProvider::Console])]
 	pub providers: Vec<NotifierProvider>,
-}
-
-// by default enable console notifier
-impl Default for ConfigNotifiers {
-	fn default() -> Self {
-		ConfigNotifiers {
-			providers: vec![NotifierProvider::Console],
-		}
-	}
 }
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
@@ -103,5 +101,3 @@ pub enum NotifierProvider {
 		subject: String,
 	},
 }
-
-fn _true() -> bool { true }
